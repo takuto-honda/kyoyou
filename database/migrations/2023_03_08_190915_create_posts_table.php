52c583d83afc971bf68f020e7072431fb55f944e
takuto-honda <takuto.engineer@gmail.com>
2023-03-09 13:09:59 +0900

    users、postsテーブル、モデルの作成

---
+++ database/migrations/2023_03_08_190915_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('title', 255)->nullable(false);
+            $table->text('body')->nullable(false);
+            $table->string('image_url')->nullable();
+            $table->enum('type', [1, 2, 3])->nullable(false);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
